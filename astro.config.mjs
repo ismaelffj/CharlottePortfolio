b6719a7027d81f6ddabb93b43459175709260f18
Ismael Florentino <ismaelffj@users.noreply.github.com>
2026-09-18 16:41:02 -0400

    Scaffold Astro project with Keystatic

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
+
+const productionUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined;
+
+export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL ?? productionUrl ?? 'http://localhost:4877',
+  output: 'static',
+  adapter: vercel(),
+  integrations: [react(), markdoc(), keystatic(), sitemap()],
+  server: { port: 4877 },
+});
---
+++ keystatic.config.ts
@@ -0,0 +1,6 @@
+/// <reference types="astro/client" />
+import { config } from '@keystatic/core';
+
+export default config({
+  storage: { kind: 'local' },
+});
